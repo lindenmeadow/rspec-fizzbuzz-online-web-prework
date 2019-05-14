02fdac3c2633f9414772690f21a19a39eda74f61
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2019-05-14 17:06:18 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if int % 3 == 0
-    puts "Fizz"
+  if int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
+  elsif int % 3 == 0
+    "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
-  elsif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    "Buzz"
   else
+    nil
 end
